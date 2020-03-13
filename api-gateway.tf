b74355bccf55f450ec36d09181f3b21a36235404
Ennio Mara <enniomara1@gmail.com>
2020-03-14 00:14:16 +0100

    Cleanup unneded resource in apigateway

--- api-gateway.tf
+++ api-gateway.tf
@@ -31,27 +31,9 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 
 }
 
-resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_method.proxy_root.resource_id
-  http_method = aws_api_gateway_method.proxy_root.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.test_lambda.invoke_arn
-}
-
 resource "aws_api_gateway_deployment" "example" {
   depends_on = [
     aws_api_gateway_integration.lambda_integration,
-    aws_api_gateway_integration.lambda_root,
   ]
 
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
