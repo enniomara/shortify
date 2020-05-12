96874fd2d05f0238e4d6865c8f17a58ccd7ebffc
Ennio Mara <enniomara1@gmail.com>
2020-05-12 15:48:02 +0200

    Use base64 hash instead of sha256 hash in lambda_function
    When using sha256 hash, terraform/AWS converts it to base64, and then
    stores it. This poses a problem next time terraform apply is ran, as the
    lambda function's source_code_hash is set to the base64 version of the
    hash, while our hash is the sha256 hash. This forced an update of the
    source code every time terraform apply ran.
    
    This change fixes this problem.

--- lambda.tf
+++ lambda.tf
@@ -17,7 +17,7 @@ resource "aws_lambda_function" "test_lambda" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = data.archive_file.service.output_sha
+  source_code_hash = data.archive_file.service.output_base64sha256
   runtime          = "python3.8"
   timeout          = 1
 }
@@ -31,7 +31,7 @@ resource "aws_lambda_function" "_entries" {
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = data.archive_file.service.output_sha
+  source_code_hash = data.archive_file.service.output_base64sha256
   runtime          = "python3.8"
   timeout          = 1
 }
