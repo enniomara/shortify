5b1e01db47b5f519e00fe41b20794c23fde974ce
Ennio Mara <enniomara@users.noreply.github.com>
2020-12-30 17:52:10 +0100

    Add tests for _entries endpoint

---
+++ tests/entries/test_entries.py
@@ -0,0 +1,42 @@
+import json
+
+import src.shortify._entries as _entries
+import tests.helpers
+
+
+def test_lambda_handler(create_table, dynamodb):
+    items = [
+        {
+            'name': 'test',
+            'location': 'http://example.com'
+        },
+        {
+            'name': 'name-a',
+            'location': 'http://example.com/abc'
+        },
+        {
+            'name': 'A1',
+            'location': 'http://example.com/a'
+        }
+    ]
+
+    # seed the table with example items
+    table = dynamodb.Table('Shortify')
+    with table.batch_writer() as batch:
+        for item in items:
+            batch.put_item(Item={
+                'name': item['name'].lower(),
+                'location': item['location']
+            })
+
+
+    apigw_event = tests.helpers.make_apigateway_event('_entries')
+    response = _entries.handler(event=apigw_event, context={})
+    body = json.loads(response['body'])
+
+    # lower because the database stores them in lower-case
+    prior_names = [i['name'].lower() for i in items]
+
+    assert response['statusCode'] == '200'
+    for item in body:
+        assert item['name'] in prior_names
