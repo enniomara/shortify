3f9e3775197def8e7d6cf58e525aa50ab9e91ef6
Ennio Mara <enniomara@users.noreply.github.com>
2021-01-03 17:29:02 +0100

    Move lambda handlers to one file

--- src/shortify/_entries.py
+++
@@ -1,7 +0,0 @@
-from . import common, entries
-
-table = common.initialize_table()
-
-
-def handler(event, context):
-    return entries.get_all_shortcut_names(event=event, table=table)
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "test_lambda" {
   filename      = local.lambda_zip_location
   function_name = "shortify-path"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "shortify.service.path_handler"
+  handler       = "shortify.handlers.path_handler"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
@@ -26,7 +26,7 @@ resource "aws_lambda_function" "_entries" {
   filename      = local.lambda_zip_location
   function_name = "shortify-entries"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "shortify.service.entries_handler"
+  handler       = "shortify.handlers.entries_handler"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
