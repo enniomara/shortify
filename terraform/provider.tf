dc81b3cff7c7e2cafd85374e300586df6f4b2b22
Ennio Mara <enniomara@users.noreply.github.com>
2021-06-27 20:46:14 +0200

    Expose a way to specify a role terraform can assume
    By adding this, the credentials that terraform is called with no longer
    need to have access to all resources. Instead they are only required to
    have access to the role that provides that access. -> Better security
    
    To ahve backward compatibility with current use (where the credentials
    that call terraform have access to all resources), no role is assumed
    when the default terraform workspace is used.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "eu-north-1"
+  assume_role {
+    role_arn = var.workspace_iam_role_arn[terraform.workspace]
+  }
+}
+
+variable "workspace_iam_role_arn" {
+  default = {
+    default = null # No ARN is assumed here in order to have a simple "terraform up" when first starting.
+    staging = "abc"
+  }
 }
