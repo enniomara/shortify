8776e9b0c063b2bef66b22c8e1bb1f971359977c
Ennio Mara <enniomara@users.noreply.github.com>
2022-02-06 17:11:19 +0100

    Rename role arn variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
-variable "workspace_iam_role_arn" {
+variable "workspace_assume_role_arn" {
   default = {
     default = null # No ARN is assumed here in order to have a simple "terraform up" when first starting.
     staging = "abc"
