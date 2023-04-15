87831d7c08d1335859412b3c322eb3f5666d8361
Ennio Mara <enniomara@users.noreply.github.com>
2023-04-15 21:00:23 +0200

    Move terraform files to repo root
    So that the repo can be imported as a module

--- lambda.tf
+++ lambda.tf
@@ -4,7 +4,7 @@ locals {
 
 data "archive_file" "service" {
   type        = "zip"
-  source_dir  = "../src/"
+  source_dir  = "./src/"
   output_path = local.lambda_zip_location
   excludes    = ["__pycache__", ".pytest_cache"]
 }
