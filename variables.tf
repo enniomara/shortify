8334aaa518bbfd7c9e12f0b26a861d1377ff1df6
Ennio Mara <enniomara@users.noreply.github.com>
2023-04-15 22:40:56 +0200

    Get rid of the old backend
    The old backend forced users to use my specific type of backend (s3).
    With the move of shortify to be defined as a module, the backend is
    no longer this repo's business.
    
    Part of #18

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "workspace_assume_role_arn" {
-  description = "For each Terraform workspace, contains the ARN with which Terraform will assume before provisioning infrastructure"
-  type        = map(string)
-  default = {
-    default = null # No ARN is assumed here in order to have a simple "terraform up" when first starting.
-    staging = "abc"
-  }
-}
